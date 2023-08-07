8677c02a50ae9740ae0cb881456bcdd21811ec3e
Clemens Wasser <clemens.wasser@gmail.com>
2023-08-07 17:59:25 +0200

    Add tests for unscrambling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io::Write};
 
 use itertools::Itertools;
 
@@ -47,6 +47,107 @@ fn find_word_candidates(
 const SPECIAL_CHARS_PRE: [char; 2] = ['(', '„'];
 const SPECIAL_CHARS_POST: [char; 7] = [',', '.', ')', '“', ':', '-', '?'];
 
+static DICTIONARY: once_cell::sync::Lazy<HashMap<u64, Vec<String>>> =
+    once_cell::sync::Lazy::new(|| {
+        let dictionary = include_str!("german.dic");
+
+        let mut dictionary_map = HashMap::<u64, Vec<String>>::new();
+
+        dictionary.lines().for_each(|word| {
+            dictionary_map
+                .entry(associative_hash(word.as_bytes()))
+                .or_default()
+                .push(word.to_string());
+        });
+
+        dictionary_map
+    });
+
+static SPECIAL_CHARS: once_cell::sync::Lazy<Vec<char>> = once_cell::sync::Lazy::new(|| {
+    SPECIAL_CHARS_PRE
+        .iter()
+        .chain(SPECIAL_CHARS_POST.iter())
+        .copied()
+        .collect()
+});
+
+fn unscramble_line(scrambled_line: &str, output: &mut impl Write) {
+    let mut first_word = true;
+    for scrambled_word in scrambled_line.split_ascii_whitespace() {
+        if !first_word {
+            write!(output, " ").unwrap();
+        }
+
+        let clean_scrambled_word = scrambled_word
+            .chars()
+            .filter(|c| !SPECIAL_CHARS.contains(c))
+            .collect::<String>();
+
+        let word_candidates = {
+            let mut word_candidates =
+                find_word_candidates(&DICTIONARY, &clean_scrambled_word, None);
+
+            if word_candidates.is_empty() {
+                word_candidates = find_word_candidates(
+                    &DICTIONARY,
+                    &clean_scrambled_word.to_lowercase(),
+                    clean_scrambled_word.chars().find(|c| c.is_uppercase()),
+                );
+
+                for word in &mut word_candidates {
+                    if let Some(first_char) = word.get_mut(..1) {
+                        first_char.make_ascii_uppercase();
+                    }
+                }
+            }
+            word_candidates
+        };
+
+        scrambled_word
+            .chars()
+            .filter(|c| SPECIAL_CHARS_PRE.contains(c))
+            .for_each(|prefix| {
+                write!(output, "{prefix}").unwrap();
+            });
+
+        match &word_candidates[..] {
+            [] => {
+                let mut chars = clean_scrambled_word.chars().collect::<Vec<_>>();
+                chars.sort_unstable();
+                write!(output, "`{}`", chars.into_iter().collect::<String>()).unwrap();
+            }
+            [word_candidate] => {
+                write!(output, "{word_candidate}").unwrap();
+            }
+            word_candidates => {
+                write!(output, "{word_candidates:?}").unwrap();
+            }
+        };
+
+        scrambled_word
+            .chars()
+            .filter(|c| SPECIAL_CHARS_POST.contains(c))
+            .for_each(|postfix| {
+                write!(output, "{postfix}").unwrap();
+            });
+
+        first_word = false;
+    }
+}
+
+fn unscramble(scrambled_string: &str, output: &mut impl Write) {
+    let mut first_line = true;
+    for scrambled_line in scrambled_string.lines() {
+        if !first_line {
+            writeln!(output).unwrap();
+        }
+
+        unscramble_line(scrambled_line, output);
+
+        first_line = false;
+    }
+}
+
 fn main() {
     let Some(filepath) = std::env::args()
         .nth(1)
@@ -56,95 +157,19 @@ fn main() {
             return;
         };
 
-    let dictionary = include_str!("german.dic");
-
-    let mut dictionary_map = HashMap::<u64, Vec<String>>::new();
-
-    dictionary.lines().for_each(|word| {
-        dictionary_map
-            .entry(associative_hash(word.as_bytes()))
-            .or_default()
-            .push(word.to_string());
-    });
-
     // Print debug stats about the used associative string hash
-    //dbg!(dictionary_map.keys().count());
-    //dbg!(dictionary_map.values().map(|words| words.len()).min());
-    //dbg!(dictionary_map.values().map(|words| words.len()).max());
-    //dbg!(dictionary_map.values().map(|words| words.len()).sum::<usize>() / dictionary_map.values().count());
+    //dbg!(DICTIONARY.keys().count());
+    //dbg!(DICTIONARY.values().map(|words| words.len()).min());
+    //dbg!(DICTIONARY.values().map(|words| words.len()).max());
+    //dbg!(DICTIONARY.values().map(|words| words.len()).sum::<usize>() / DICTIONARY.values().count());
 
-    let encrypted_news = std::fs::read_to_string(filepath).expect("Failed to read in file!");
-
-    let special_chars: Vec<_> = SPECIAL_CHARS_PRE
-        .iter()
-        .chain(SPECIAL_CHARS_POST.iter())
-        .copied()
-        .collect();
-
-    encrypted_news.lines().for_each(|line| {
-        line.split_ascii_whitespace().for_each(|encrypted_word| {
-            let clean_encrypted_word = encrypted_word
-                .chars()
-                .filter(|c| !special_chars.contains(c))
-                .collect::<String>();
-
-            let word_candidates = {
-                let mut word_candidates =
-                    find_word_candidates(&dictionary_map, &clean_encrypted_word, None);
-
-                if word_candidates.is_empty() {
-                    word_candidates = find_word_candidates(
-                        &dictionary_map,
-                        &clean_encrypted_word.to_lowercase(),
-                        clean_encrypted_word.chars().find(|c| c.is_uppercase()),
-                    );
-
-                    for word in &mut word_candidates {
-                        if let Some(first_char) = word.get_mut(..1) {
-                            first_char.make_ascii_uppercase();
-                        }
-                    }
-                }
-                word_candidates
-            };
-
-            encrypted_word
-                .chars()
-                .filter(|c| SPECIAL_CHARS_PRE.contains(c))
-                .for_each(|prefix| {
-                    print!("{prefix}");
-                });
-
-            match &word_candidates[..] {
-                [] => {
-                    let mut chars = clean_encrypted_word.chars().collect::<Vec<_>>();
-                    chars.sort_unstable();
-                    print!("`{}`", chars.into_iter().collect::<String>());
-                }
-                [word_candidate] => {
-                    print!("{word_candidate}");
-                }
-                word_candidates => {
-                    print!("{word_candidates:?}");
-                }
-            };
-
-            encrypted_word
-                .chars()
-                .filter(|c| SPECIAL_CHARS_POST.contains(c))
-                .for_each(|postfix| {
-                    print!("{postfix}");
-                });
-
-            print!(" ");
-        });
-        println!();
-    });
+    let scrambled_string = std::fs::read_to_string(filepath).expect("Failed to read in file!");
+    unscramble(&scrambled_string, &mut std::io::stdout());
 }
 
 #[cfg(test)]
 mod test {
-    use super::associative_hash;
+    use super::*;
 
     #[test]
     fn test_associative_hash() {
@@ -155,5 +180,14 @@ mod test {
             associative_hash(TEST_STRING),
             associative_hash(&sorted_string)
         );
+    }
+
+    #[test]
+    fn test_unscrable() {
+        const TEST_STRING: &str = "eiD rüedW dse cnnesheM its atusar.tanbn eSi uz eahntc dnu uz shcenztü tis tincufhpegrlV ealrl iesnatclhat .eawltG";
+        const EXPECTED_STRING: &str = r#"Die Würde des Menschen ist unantastbar. ["Sei", "Sie"] zu achten und zu schützen ist Verpflichtung aller ["atlantische", "staatlichen"] Gewalt."#;
+        let mut output_string = Vec::new();
+        unscramble(TEST_STRING, &mut output_string);
+        assert_eq!(String::from_utf8(output_string).unwrap(), EXPECTED_STRING);
     }
 }
